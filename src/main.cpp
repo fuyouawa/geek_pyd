cb4d7cf60e536720858b02336a8051f699582677
计都 <119787492+fuyouawa@users.noreply.github.com>
2024-11-03 16:53:39 +0800

    初始化

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,27 @@
 #include <pybind11/pybind11.h>
+#include <geek/process/process.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
-}
-
 namespace py = pybind11;
 
-PYBIND11_MODULE(cmake_example, m) {
+PYBIND11_MODULE(geek, m) {
     m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-
-        .. currentmodule:: cmake_example
-
-        .. autosummary::
-           :toctree: _generate
-
-           add
-           subtract
     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
+    m.def("test", [](int jj) {});
 
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
+    py::class_<geek::Address>(m, "Address")
+        .def(py::init<geek::Process*, uint64_t>(), py::arg("proc"), py::arg("addr"))
+        .def("read", static_cast<std::optional<std::vector<uint8_t>>(geek::Address::*)(size_t) const>(&geek::Address::Read), py::arg("size"))
+        .def("write", &geek::Address::Write, py::arg("buf"), py::arg("len"), py::arg("force") = false)
+        .def("addr", &geek::Address::addr);
+    
+    py::class_<geek::Process>(m, "Process")
+        .def("at", &geek::Process::At);
+    
+    m.attr("this_proc") = geek::ThisProcess;
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
